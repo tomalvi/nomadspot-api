100886425526e9fac05acebee180aaf10c528f04
tomalvi <gonvineglas@gmail.com>
2026-06-08 15:54:24 +0200

    deploy

--- config/database.php
+++ config/database.php
@@ -59,8 +59,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
                 1011 => 'mysql_native_password', 
             ]) : [],
