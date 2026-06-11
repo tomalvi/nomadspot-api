453e9bf7636e7adac13c179f40bac790b3c9f9c1
tomalvi <gonvineglas@gmail.com>
2026-06-11 14:56:04 +0200

    render bug

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# 🔥 Forzar la creación de tablas e importación al arrancar
-echo "🚀 Ejecutando migraciones..."
-php artisan migrate --force
-
-echo "📥 Importando datos de ciudades..."
-# php artisan data:import
-
 echo "⚡ Optimizando caché de Laravel..."
 php artisan config:clear
 php artisan optimize
